2ef872682a18e3a2f8dfc3f278f467c0d17ec10d
DO_S <rlaxogus7094@naver.com>
2024-07-20 09:00:43 +0900

    design : 디자인시스템 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
-  title: "급식 클립보드!",
-  description: "매크로 사용을 위한 급식 클립보드",
+  title: "급식 클립보드 - Meal Clipboard",
+  description: "광양고등학교 인스타그램 운영 지원을 위한 급식 클립보드",
 };
 
 export default function RootLayout({
@@ -16,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
